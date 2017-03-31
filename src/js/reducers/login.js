0c2d704f3ff180182248d3323dd8d91b161c2223
Q <coolqi92@gmail.com>
2017-03-31 14:03:56 +0800

    fix redux

--- src/js/reducers/login.js
+++ src/js/reducers/login.js
@@ -8,15 +8,15 @@ const loginReducer = (state = loginState, action) => {
 	state = state || loginState;
 	switch( action.type ) {
 		case types.LOGIN:
-			return Object.assign([], state, {
+			return Object.assign({}, state, {
 				token: action.payload.token
 			})
 		case types.SAVE_USERNAME:			
-			return Object.assign([], state, {
+			return Object.assign({}, state, {
 				username: action.payload.username
 			})
 		case types.LOG_OUT:
-			return Object.assign([], state, {
+			return Object.assign({}, state, {
 				username: '',
 				token: ''
 			})
